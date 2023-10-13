1292c4f048bc2fb34aa489eaa21a8912130189b7
Leonardo Tegarsuan <2001020019@student.umrah.ac.id>
2023-10-13 03:05:29 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,46 @@
-const http = require('http');
+// Create web server
+// 1. Create a web server
+// 2. Create a route for GET /comments
+// 3. Create a route for POST /comments
+// 4. Create a route for GET /comments/:id
+// 5. Create a route for PUT /comments/:id
+// 6. Create a route for DELETE /comments/:id
+// 7. Start the web server
+// 8. Test the web server using Postman
 
-const hostname = '127.0.0.1';
+// 1. Create a web server
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+// 2. Create a route for GET /comments
+app.get('/comments', (req, res) => {
+  res.send('GET /comments');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// 3. Create a route for POST /comments
+app.post('/comments', (req, res) => {
+  res.send('POST /comments');
 });
+
+// 4. Create a route for GET /comments/:id
+app.get('/comments/:id', (req, res) => {
+  res.send('GET /comments/:id');
+});
+
+// 5. Create a route for PUT /comments/:id
+app.put('/comments/:id', (req, res) => {
+  res.send('PUT /comments/:id');
+});
+
+// 6. Create a route for DELETE /comments/:id
+app.delete('/comments/:id', (req, res) => {
+  res.send('DELETE /comments/:id');
+});
+
+// 7. Start the web server
+app.listen(port, () => {
+  console.log(`Web server is listening at http://localhost:${port}`);
+});
+
+// 8. Test the web server using Postman
